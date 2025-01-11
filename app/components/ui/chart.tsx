a3cf16ca3f4b881a6f50d19f7a16a4c57ff78bca
uchechi14 <uchechinnorom9@gmail.com>
2025-01-11 15:51:16 +0100

    update

--- app/components/ui/chart.tsx
+++ app/components/ui/chart.tsx
@@ -1,57 +1,57 @@
-'use client'
+// 'use client'
 
-import { useState } from "react";
-import dynamic from "next/dynamic";
-import { ApexOptions } from "apexcharts";
+// import { useState } from "react";
+// import dynamic from "next/dynamic";
+// import { ApexOptions } from "apexcharts";
 
 
 
-const ReactApexChart = dynamic(() => import('react-apexcharts'), {
-    ssr: false
-});
+// const ReactApexChart = dynamic(() => import('react-apexcharts'), {
+//     ssr: false
+// });
 
-interface ChartState {
-    series: number[],
-    options: ApexOptions;
-}
+// interface ChartState {
+//     series: number[],
+//     options: ApexOptions;
+// }
 
-const ApexChart = () => {
-    const [state] = useState<ChartState>({
+// const ApexChart = () => {
+//     const [state] = useState<ChartState>({
       
-        series: [44, 55, 41, 17, 15],
-        options: {
-          chart: {
-            type: 'donut',
-          },
-          responsive: [{
-            breakpoint: 480,
-            options: {
-              chart: {
-                width: 200
-              },
-              legend: {
-                position: 'bottom'
-              }
-            }
-          }]
-        },
+//         series: [44, 55, 41, 17, 15],
+//         options: {
+//           chart: {
+//             type: 'donut',
+//           },
+//           responsive: [{
+//             breakpoint: 480,
+//             options: {
+//               chart: {
+//                 width: 200
+//               },
+//               legend: {
+//                 position: 'bottom'
+//               }
+//             }
+//           }]
+//         },
       
       
-    });
+//     });
 
     
 
-    return (
-      <div className="w-full">
-        <div id="chart">
-            <ReactApexChart 
-            options={state.options} 
-            series={state.series} 
-            type="donut" 
-            height={350} />
-          </div>
-      </div>
-    );
-  }
+//     return (
+//       <div className="w-full">
+//         <div id="chart">
+//             <ReactApexChart 
+//             options={state.options} 
+//             series={state.series} 
+//             type="donut" 
+//             height={350} />
+//           </div>
+//       </div>
+//     );
+//   }
 
-export default ApexChart;
+// export default ApexChart;
